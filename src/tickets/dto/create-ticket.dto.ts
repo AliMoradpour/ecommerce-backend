0ea3baa4b6f1c937d1ae4ee4263df36921de3d53
AliMoradpour <mr.mrdp@yahoo.com>
2026-07-08 22:29:59 +0330

    ticket dto

--- src/tickets/dto/create-ticket.dto.ts
+++ src/tickets/dto/create-ticket.dto.ts
@@ -1,1 +1,21 @@
-export class CreateTicketDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateTicketDto {
+  @IsNotEmpty({ message: 'آیدی کاربر نمیتواند خالی باشد' })
+  userId!: number;
+
+  @IsNotEmpty({ message: 'عنوان نمیتواند خالی باشد' })
+  @IsString({ message: 'عنوان باید رشته باشد' })
+  title!: string;
+
+  @IsNotEmpty({ message: 'موضوع نمیتواند خالی باشد' })
+  @IsString({ message: 'موضوع باید رشته باشد' })
+  subject!: string;
+
+  @IsNotEmpty({ message: 'توضیحات نمیتواند خالی باشد' })
+  @IsString({ message: 'توضیحات باید رشته باشد' })
+  description!: string;
+
+  @IsOptional()
+  replyTo?: number;
+}
