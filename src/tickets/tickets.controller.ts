a87eec665c5e69d1de764150b2363e14e8e96fbe
AliMoradpour <mr.mrdp@yahoo.com>
2026-07-08 22:48:53 +0330

    create ticket controller

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,34 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { CreateTicketDto } from './dto/create-ticket.dto';
-import { UpdateTicketDto } from './dto/update-ticket.dto';
+import express from 'express';
 
 @Controller('tickets')
 export class TicketsController {
   constructor(private readonly ticketsService: TicketsService) {}
 
   @Post()
-  create(@Body() createTicketDto: CreateTicketDto) {
-    return this.ticketsService.create(createTicketDto);
-  }
+  async create(
+    @Res() res: express.Response,
+    @Body() createTicketDto: CreateTicketDto,
+  ) {
+    const newTicket = await this.ticketsService.create(createTicketDto);
 
-  @Get()
-  findAll() {
-    return this.ticketsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ticketsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTicketDto: UpdateTicketDto) {
-    return this.ticketsService.update(+id, updateTicketDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ticketsService.remove(+id);
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      data: newTicket,
+      message: 'تیکت ایجاد شد',
+    });
   }
 }
