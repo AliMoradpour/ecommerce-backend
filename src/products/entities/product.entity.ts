fd3ee509bebf08b3307a89f8222667b1dfbcc6cc
AliMoradpour <mr.mrdp@yahoo.com>
2026-07-09 15:38:09 +0330

    products entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,28 @@
-export class Product {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  title: string;
+
+  @Column({ nullable: false })
+  price: number;
+
+  @Column({ nullable: false })
+  stock: number;
+
+  @Column({ nullable: false })
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToMany(() => Category, (category) => category.products)
+  categories: Category[];
+}
