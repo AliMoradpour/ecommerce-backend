7112b90e985deb6b383f176dbc2a85327c02a0c7
AliMoradpour <mr.mrdp@yahoo.com>
2026-07-12 12:31:36 +0330

    category controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,18 +1,31 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Res, HttpStatus } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import express from 'express';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  async create(
+    @Res() res: express.Response,
+    @Body() createCategoryDto: CreateCategoryDto,
+  ) {
+    const category = await this.categoriesService.create(createCategoryDto);
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      data: category,
+      message: 'دسته‌بندی با موفقیت ساخته شد',
+    });
   }
 
   @Get()
-  findAll() {
-    return this.categoriesService.findAll();
+  async findAll(@Res() res: express.Response) {
+    await this.categoriesService.findAll();
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'لیست دسته‌بندی ها با موفقیت پیدا شد',
+    });
   }
 }
